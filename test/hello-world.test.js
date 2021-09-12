64a322358e9f89abd305f09a8e92d726d242b9fc
Greg Farrow <g-farrow@users.noreply.github.com>
2021-09-12 15:23:49 +0100

    initial commit

--- test/hello-world.test.js
+++ test/hello-world.test.js
@@ -1,22 +1,24 @@
-import { remote } from 'webdriverio';
-import { assert } from 'chai';
-describe('Hello world', function () {
-    it('Making the tests work', async function () {
+import { remote } from "webdriverio";
+import { assert } from "chai";
+import { setupBrowser } from "@testing-library/webdriverio";
+describe("Hello world", function () {
+    it("Making the tests work", async function () {
         const client = await remote({
             capabilities: {
-                automationName: 'uiautomator2',
-                platformName: 'android',
-                deviceName: 'RF8M21KYYEH',
-                browserName: 'chrome'
+                automationName: "uiautomator2",
+                platformName: "android",
+                deviceName: "RF8M21KYYEH",
+                browserName: "chrome",
             },
-            path: '/wd/hub',
-            port: parseInt(process.env.APPIUM_PORT || '4723'),
-            logLevel: 'info'
+            path: "/wd/hub",
+            port: parseInt(process.env.APPIUM_PORT || "4723"),
+            logLevel: "info",
         });
-        await client.url('https://www.google.com');
+        const browser = setupBrowser(client);
+        await client.url("https://www.google.com");
         const title = await client.getTitle();
-        assert.equal(title, 'Google');
-        console.log('Online check completed');
+        assert.equal(title, "Google");
+        browser.getByText("Google");
         await client.deleteSession();
     });
 });
